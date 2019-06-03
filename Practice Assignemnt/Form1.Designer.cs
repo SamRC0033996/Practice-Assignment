b93b522fa477d4f0012d303ca62f480b374d7df5
Sam Titterington <rc0033996@student.riversidecollege.ac.uk>
2019-06-03 14:37:09 +0100

    Add project files.

---
+++ Practice Assignemnt/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Practice_Assignemnt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.lblLength = new System.Windows.Forms.Label();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(231, 97);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(231, 139);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Location = new System.Drawing.Point(126, 97);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(32, 13);
+            this.lblWidth.TabIndex = 2;
+            this.lblWidth.Text = "width";
+            this.lblWidth.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Location = new System.Drawing.Point(129, 139);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(36, 13);
+            this.lblLength.TabIndex = 3;
+            this.lblLength.Text = "length";
+            this.lblLength.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.Location = new System.Drawing.Point(231, 178);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(28, 13);
+            this.lblArea.TabIndex = 4;
+            this.lblArea.Text = "area";
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(495, 167);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(75, 23);
+            this.Calculate.TabIndex = 5;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.lblLength);
+            this.Controls.Add(this.lblWidth);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.Button Calculate;
+    }
+}
+
